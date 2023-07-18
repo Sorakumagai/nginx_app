644a98ef798eaf1e596102bd8e64286be0292b73
sora.kumagai@ntt-at.co.jp <sora.199931212@gmail.com>
2023-07-19 06:49:28 +0900

    first commit

---
+++ flask_api/app/api/api_test.py
@@ -0,0 +1,23 @@
+from flask import Blueprint,request,jsonify
+# BluePrint參數設定
+api_test = Blueprint('api_test', __name__)
+# ***********************************************
+# Create GET http://IP:5000/
+# ***********************************************
+@api_test.route('/hi',methods=['GET'])
+def index():  
+  return "hi"
+
+# *****************************************************************************************
+# Create POST http://IP:5000/post_name
+# json
+# Key: name
+# Value: Your Name
+# *****************************************************************************************
+@api_test.route("/post_name", methods=['POST'])
+def postMethod():
+    value = request.json['name']
+    try:
+        return jsonify({"status": 200, "comment": "Hello " + value})
+    except:
+        return jsonify({"status": 200, "comment": "ERROR"})
---
+++ flask_api/run.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from app import create_app
+
+app = create_app()
+
+@app.route('/')
+def index():
+    return """
+    Hello World
+    """
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port=5000,debug=True)
+
